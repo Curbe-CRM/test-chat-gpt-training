4b7345098923b21c6d7eefb6491ca9437109fbc0
Alex Astudillo <astudilloalex1@gmail.com>
2023-08-20 12:27:19 -0500

    Test with PostgreSQL

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 * pip3 install pypdf
 * pip3 install llama-cpp-python
 * pip3 install sentence_transformers
+* pip3 install psycopg2 (PostgreSQL database adapter)
+* pip3 install flask (For rest api)
+
+#### Commands
+flask --app main run
--- main.py
+++ main.py
@@ -7,10 +7,23 @@ from llama_index import (
     GPTVectorStoreIndex,
     LLMPredictor,
     StorageContext,
-    load_index_from_storage
+    load_index_from_storage,
+    ServiceContext, SQLDatabase
 )
 import gradio as gr
 import sys
+from flask import Flask, request
+
+from llama_index.indices.struct_store import NLSQLTableQueryEngine
+from sqlalchemy import create_engine, inspect
+
+db_user = ''
+db_password = ""
+db_host = ""
+db_name = ""
+db_port = ""
+# connection_uri = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+connection_uri = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 # This is a sample Python script.
 
@@ -21,10 +34,44 @@ openai.api_key = ''
 # chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
 #                                               messages=[{"role": "user", "content": "Cual es la mejor moto del mundo"}])
 
-print('chat_completion')
+# print('chat_completion')
+#
+# engine = create_engine(connection_uri)
+# llm = OpenAI(
+#     openai_api_key='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',
+#     temperature=0.5,
+#     model_name="text-davinci-003",
+#     max_tokens=512,
+# )
+# service_context = ServiceContext.from_defaults(llm=llm)
+# sql_database = SQLDatabase(engine)
+# inspector = inspect(engine)
+# table_names = inspector.get_table_names()
+# print(table_names)
+
+# def chat_to_sql(question: str):
+#     query_engine = NLSQLTableQueryEngine(
+#         sql_database=sql_database,
+#         tables=table_names,
+#         synthesize_response=True,
+#         service_context=service_context
+#     )
+#     try:
+#         response = query_engine.query(question)
+#         response_md = str(response)
+#         sql = response.metadata['sql_query']
+#     except Exception as ex:
+#         response_md = 'Error'
+#         sql = f'ERROR: {str(ex)}'
+#     return response_md
 
 
 def construct_index(directory_path):
+    """
+    Construct index data from directory path.
+    :param directory_path: The path of the directory.
+    :return: A VectorStoreIndex.
+    """
     max_input_size = 4096
     num_outputs = 512
     max_chunk_overlap = 1
@@ -40,7 +87,7 @@ def construct_index(directory_path):
         llm=OpenAI(
             openai_api_key='',
             temperature=0.5,
-            odel_name="text-davinci-003",
+            model_name="text-davinci-003",
             max_tokens=num_outputs,
         )
     )
@@ -68,12 +115,20 @@ def chatbot(input_text):
     return response.query(input_text).response
 
 
-iface = gr.Interface(fn=chatbot,
-                     inputs=gr.inputs.Textbox(lines=3, label="Enter your text"),
-                     outputs="text",
-                     title="My AI Chatbot")
+# iface = gr.Interface(fn=chatbot,
+ #                    inputs=gr.inputs.Textbox(lines=3, label="Enter your text"),
+  #                  outputs="text",
+ #                    title="My AI Chatbot")
 
-index = construct_index("docs")
-iface.launch(share=True)
+# construct_index("docs")
+#iface.launch(share=True)
 
 # print(construct_index('docs'))
+
+app = Flask(__name__)
+
+
+@app.route('/bot-question', methods=['POST'])
+def response_question():
+    question = request.get_json()
+    return chatbot(question['question'])
