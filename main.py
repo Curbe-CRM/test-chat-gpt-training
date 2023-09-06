e60c22dfe297d67adff929653199c63844c00dab
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2023-09-06 12:14:01 -0500

    implementacion modelo mejorado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 import os
-from langchain.document_loaders import (PyPDFLoader,CSVLoader)
+from langchain.document_loaders import (PyPDFLoader,CSVLoader,TextLoader)
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -13,14 +13,15 @@ from flask import (Flask, redirect, render_template, request,send_from_directory
 import json
 from flask_cors import CORS
 
-def saveModelJSON():
-    os.environ["OPENAI_API_KEY"] = "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"
-    loader = CSVLoader(file_path='data/csv/convertcsv.csv',csv_args={"delimiter":"|"})
+def saveModelDoc():
+    os.environ["OPENAI_API_KEY"] = "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"
+    loader = TextLoader('data/indice/serviciosdetalle.txt')
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     data=loader.load()    
-    texts = text_splitter.split_documents(data)    
-    embeddings = OpenAIEmbeddings(model='text-embedding-ada-002') 
-    Chroma.from_documents(texts, embeddings,persist_directory="./model")
+    texts = text_splitter.split_documents(data)
+    print(texts[1])
+    # embeddings = OpenAIEmbeddings(model='text-embedding-ada-002')    
+    # Chroma.from_documents(texts, embeddings,persist_directory="./model")
 
 def saveModel():
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -34,31 +35,31 @@ def saveModel():
     docsearch = Chroma.from_documents(texts, embeddings, metadatas=[{"source": str(i)} for i in range(len(texts))],persist_directory="./model")    
 
 def queryModel(question):    
-    os.environ["OPENAI_API_KEY"] = "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"
+    os.environ["OPENAI_API_KEY"] = "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"
     db3 = Chroma(persist_directory="./model",embedding_function=OpenAIEmbeddings(model='text-embedding-ada-002'))
     docs = db3.similarity_search(question)
     chain = load_qa_chain(ChatOpenAI(temperature=1,model_name='gpt-3.5-turbo',max_tokens=1000), 
                         chain_type="stuff")    
     response=chain.run(input_documents=docs, question=question)
-    responseObj={
-        "ref_number_client": "593967585156",
-    "ref_number_company": "593967916241",
-    "ref_user_name": "Stalin Figueroa",
-    "ref_type": "text",
-    "ref_data": {
-        "ref_payload": None,
-        "ref_text": response
-    },
-    "ref_date": "2022-11-21T00:24:36.000Z"
-    }
-    return responseObj
-saveModelJSON()
-app = Flask(__name__)
-CORS(app)
-@app.route('/bot-question', methods=['POST'])
-def response_question():
-    question = request.get_json()
-    return queryModel(question['question'])
+    # responseObj={
+    #     "ref_number_client": "593967585156",
+    # "ref_number_company": "593967916241",
+    # "ref_user_name": "Stalin Figueroa",
+    # "ref_type": "text",
+    # "ref_data": {
+    #     "ref_payload": None,
+    #     "ref_text": response
+    # },
+    # "ref_date": "2022-11-21T00:24:36.000Z"
+    # }
+    return response
+saveModelDoc()
+# app = Flask(__name__)
+# CORS(app)
+# @app.route('/bot-question', methods=['POST'])
+# def response_question():
+#     question = request.get_json()
+#     return queryModel(question['question'])
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',port=5000)
--- second.py
+++ second.py
@@ -88,7 +88,7 @@ def construct_index(directory_path):
     )
     llm_predictor = LLMPredictor(
         llm=ChatOpenAI(
-            openai_api_key='',
+            openai_api_key='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',
             temperature=1,
             model_name="gpt-3.5",
             max_tokens=num_outputs,
